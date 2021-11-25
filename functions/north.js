864918e936260ec90cc234580b63ea72fa84c0d4
Shivppatel <shiv.p.patel@outlook.com>
2021-11-25 15:18:30 -0500

    testing cros issues in prod

--- functions/north.js
+++ functions/north.js
@@ -3,6 +3,9 @@ exports.handler = async function (event, context) {
   var data = await purpleairHandler.getUpdatedNorthCountySensorsData();
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*' // Allow from anywhere
+    },
     body: JSON.stringify(data)
   };
 };
