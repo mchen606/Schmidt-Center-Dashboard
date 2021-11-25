e8baba3c36eb11307f84d5450e2b6d701edcf7ea
Shivppatel <shiv.p.patel@outlook.com>
2021-11-24 20:41:55 -0500

    Added routes to test processed

---
+++ netlify/functions/index.js
@@ -0,0 +1,10 @@
+var sensors_controller = require('../handlers/listOfSensorsIDs');
+
+/* GET home page. */
+exports.handler = async function (event, context) {
+  const data = await sensors_controller.listSensors();
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: data })
+  };
+};
