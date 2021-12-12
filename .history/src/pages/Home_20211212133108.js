9868f2886ef90ae542a32596c6fb34d65e7a0cf8
arkomz <arkomz91@gmail.com>
2021-12-12 13:34:28 -0500

    Home Page fixes

--- .history/src/pages/Home_20211212133108.js
+++ .history/src/pages/Home_20211212133108.js
@@ -44,6 +44,18 @@ function Home() {
                 <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
                   Schmidt Center Dashboard
                 </p>
+                <p className='mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto'>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
               </div>
 
               <div className='mt-10'>
