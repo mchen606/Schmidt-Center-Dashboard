f74a0edbdcc025f639e6043c33873c99b2b6ca38
Antoine Vignon <vignonantoinem@gmail.com>
2021-12-12 21:22:03 -0500

    Delete history.js

--- functions/history.js
+++
@@ -1,9 +0,0 @@
-var thingspeakHandler = require('../handlers/thingspeakDataHandler');
-exports.handler = async function (event, context) {
-  //const input = JSON.parse(event.body);
-  var data = await thingspeakHandler.getThingspeakProcessedData();
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  };
-};
